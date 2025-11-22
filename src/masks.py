0dfcabad03102717e562d4c9e317d79cbe542d15
Рожнев Максим <kaster3190@mail.ru>
2025-11-23 00:38:38 +0700

    dd input validation and improve mask safety

--- src/masks.py
+++ src/masks.py
@@ -2,12 +2,23 @@ from typing import Union
 
 
 def get_mask_card_number(card_number: Union[int, str]):
-    """Маскирует номер банковской карты, оставляет первые 6 цифр и последнии 4, остальные меняет на *"""
+    """Маскирует номер банковской карты.
+    Оставляет первые 6 цифр и последние 4, остальные заменяет на *.
+    """
     str_card_number = str(card_number)
+
+    # Проверка, чтобы номер был не слишком коротким
+    if len(str_card_number) < 10:
+        raise ValueError("Card number must contain at least 10 digits")
+
     return f"{str_card_number[:4]} {str_card_number[4:6]}** **** {str_card_number[-4:]}"
 
 
 def get_mask_account(account: Union[int, str]):
-    """Маскирует номер счёта, оставляет последнии 4 цифры"""
+    """Маскирует номер счёта, оставляет последние 4 цифры."""
     str_account = str(account)
+
+    if len(str_account) < 4:
+        raise ValueError("Account number must contain at least 4 digits")
+
     return f"**{str_account[-4:]}"
