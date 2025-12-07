feb05cbc8e22ac199c12a9b0ef6b72d36e17e44b
maxim_rozhnev <kaster3190@mail.ru>
2025-12-07 23:15:16 +0700

    Сохраняем текущее состояние перед rebase

---
+++ tests/test_masks.py
@@ -0,0 +1,27 @@
+from src.masks import get_mask_card_number, get_mask_account
+
+
+def test_get_mask_card_number(card_number):
+    assert get_mask_card_number(card_number) == "1234 56** **** 3456"
+
+
+def test_get_mask_account(account_number):
+    assert get_mask_account(account_number) == "**4305"
+
+
+def test_get_mask_card_number_short_error():
+    try:
+        get_mask_card_number("12345")
+    except ValueError:
+        assert True
+    else:
+        assert False
+
+
+def test_get_mask_account_short_error():
+    try:
+        get_mask_account("12")
+    except ValueError:
+        assert True
+    else:
+        assert False
