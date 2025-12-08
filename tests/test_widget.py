b1db164850be750a1d39c5c2a334a0f8d5e20d5b
maxim_rozhnev <kaster3190@mail.ru>
2025-12-08 23:43:39 +0700

    Обновление кода под flake8, pypy, isort

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,13 +1,18 @@
+import pytest
+
 from src.widget import get_date, mask_account_card
 
 
-@pytest.mark.parametrize("text,expected", [
-("Visa Classic 1234567890123456", "Visa Classic 1234 56** **** 3456"),
-("Счет 73654108430135874305", "Счет **4305"),
-])
-def test_mask_account_card(text, expected):
+@pytest.mark.parametrize(
+    "text,expected",
+    [
+        ("Visa Classic 1234567890123456", "Visa Classic 1234 56** **** 3456"),
+        ("Счет 73654108430135874305", "Счет **4305"),
+    ],
+)
+def test_mask_account_card(text: str, expected: str) -> None:
     assert mask_account_card(text) == expected
 
 
-def test_get_date():
+def test_get_date() -> None:
     assert get_date("2023-10-10T12:00:00") == "10.10.2023"
