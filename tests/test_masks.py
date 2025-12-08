fc3d01e0d824f8e3fa85f12d3671749445e572bf
maxim_rozhnev <kaster3190@mail.ru>
2025-12-08 23:39:36 +0700

    Обновление тестирования с try/except на raises

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,27 +1,26 @@
+import pytest
+
 from src.masks import get_mask_account, get_mask_card_number
 
 
-def test_get_mask_card_number(card_number: str) -> None:
-    assert get_mask_card_number(card_number) == "1234 56** **** 3456"
+@pytest.mark.parametrize("card_number,expected", [
+("1234567890123456", "1234 56** **** 3456"),
+])
+def test_get_mask_card_number(card_number, expected):
+    assert get_mask_card_number(card_number) == expected
 
 
-def test_get_mask_account(account_number: str) -> None:
+def test_get_mask_account(account_number="73654108430135874305"):
     assert get_mask_account(account_number) == "**4305"
 
 
-def test_get_mask_card_number_short_error() -> None:
-    try:
-        get_mask_card_number("12345")
-    except ValueError:
-        assert True
-    else:
-        assert False
+@pytest.mark.parametrize("card_number", ["12345", "111"])
+def test_get_mask_card_number_short_error(card_number):
+    with pytest.raises(ValueError):
+        get_mask_card_number(card_number)
 
 
-def test_get_mask_account_short_error() -> None:
-    try:
-        get_mask_account("12")
-    except ValueError:
-        assert True
-    else:
-        assert False
+@pytest.mark.parametrize("acc", ["12", "1234"])
+def test_get_mask_account_short_error(acc):
+    with pytest.raises(ValueError):
+        get_mask_account(acc)
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,15 +1,13 @@
 from src.widget import get_date, mask_account_card
 
 
-def test_mask_account_card_card() -> None:
-    text = "Visa Classic 1234567890123456"
-    assert mask_account_card(text) == "Visa Classic 1234 56** **** 3456"
+@pytest.mark.parametrize("text,expected", [
+("Visa Classic 1234567890123456", "Visa Classic 1234 56** **** 3456"),
+("Счет 73654108430135874305", "Счет **4305"),
+])
+def test_mask_account_card(text, expected):
+    assert mask_account_card(text) == expected
 
 
-def test_mask_account_card_account() -> None:
-    text = "Счет 73654108430135874305"
-    assert mask_account_card(text) == "Счет **4305"
-
-
-def test_get_date() -> None:
+def test_get_date():
     assert get_date("2023-10-10T12:00:00") == "10.10.2023"
