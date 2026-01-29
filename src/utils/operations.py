2dd7ce7c927aa281ecc8deeb970a7fdbbaf8de79
maxim_rozhnev <kaster3190@mail.ru>
2026-01-29 19:23:16 +0700

    Fix bugs

--- src/utils/operations.py
+++ src/utils/operations.py
@@ -1,21 +1,19 @@
 import json
-from typing import List, Dict
+from typing import Any, Dict, List
 
 
-def load_operations(path: str) -> List[Dict]:
+def load_operations(path: str) -> List[Dict[str, Any]]:
     """
     Загружает финансовые транзакции из JSON-файла.
 
-    param path путь к JSON-файлу
-    return список словарей с транзакциями или пустой список
+    :param path: путь к JSON-файлу
+    :return: список словарей с транзакциями или пустой список
     """
     try:
         with open(path, "r", encoding="utf-8") as file:
-            data = json.load(file)
-
+            data: Any = json.load(file)
             if isinstance(data, list):
                 return data
             return []
-
     except (FileNotFoundError, json.JSONDecodeError):
         return []
